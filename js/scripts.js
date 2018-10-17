1fde37cdb43c8e54ba6666024b4097b0a85d49f9
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2018-10-17 14:20:06 -0700

    :create second function that reverses the result of the previous function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 var sentence = prompt("Please enter your sentence");
 console.log(sentence);
 
-//First function
+//First function that will capitilize the first and the last letter of the sentence
 var firstFunction = function(){
   for(i=0; i<=sentence.length; i++){
     var lastLetter = sentence.length-1;
     
     var firstResult = sentence.charAt(0).toUpperCase()+sentence.substr(lastLetter).toUpperCase();
     return firstResult;
-    
-  }
+    }
+}
+console.log(firstFunction());
+
+
+//Second function will reverse the order of 2 letters that are output of the firstFunction
+var secondFunction = function(){
   
+  var splitLetters = firstFunction().split("");
+  var reversedLetters = splitLetters.reverse().join('');
+  return reversedLetters;
 }
 
-console.log(firstFunction());
+console.log(secondFunction());
